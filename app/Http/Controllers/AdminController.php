7a6607f7849c5d837571ec5cf1df55b28031ac9e
yeaminrabbi <yeaminrabbi308@gmail.com>
2024-04-30 22:32:55 +0600

    cleanup

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,26 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Otp;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 use Illuminate\Support\Facades\Hash;
-
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\LeaveApplication;
+
 class AdminController extends Controller
 {
     function dashboard()
     {
+        if(Auth::user()->getRoleNames()[0] == 'admin')
+        {
+            $query = LeaveApplication::get();
+            $dashboardData = [
+                'pendingApplication' => $query->where('status', 0)->count(),
+                'approvedApplication' => $query->where('status', 1)->count(),
+                'rejectedApplication' => $query->where('status', -1)->count()
+            ];
+
+            return view('adminpanel.dashboard.index', compact('dashboardData'));
+        }
+        
         return view('adminpanel.dashboard.index');
     }
-
-    function otp_list(){
-
-        return view('adminpanel.otp.index',[
-            'otp_list' => Otp::orderBy('updated_at','DESC')->paginate()
-        ]);
-    }
-
 
 }
--- resources/views/adminpanel/dashboard/index.blade.php
+++ resources/views/adminpanel/dashboard/index.blade.php
@@ -10,11 +10,11 @@
                             <div class="card-body">
                                 <h5 class="card-title text-primary">Welcome {{ Auth::user()->name }}! 🎉</h5>
                                 <p class="mb-4">
-                                <?php 
-                                    $a=array('When you have a dream, you have got to grab it and never let go', 'Never give up','Life is what happens when you are busy making other plans', 'Many of life’s failures are people who did not realize how close they were to success when they gave up', 'The purpose of our lives is to be happy', 'Money and success don’t change people; they merely amplify what is already there.','Never let the fear of striking out keep you from playing the game','The big lesson in life, baby, is never be scared of anyone or anything','The whole secret of a successful life is to find out what is one’s destiny to do, and then do it.','The unexamined life is not worth living','Turn your wounds into wisdom.','Don’t settle for what life gives you; make life better and build something','Live for each second without hesitation');
-                                    $random_keys=rand(0,sizeof($a)-1);
-                                    echo $a[$random_keys];                                
-                                ?> 
+                                    <?php
+                                    $a = ['When you have a dream, you have got to grab it and never let go', 'Never give up', 'Life is what happens when you are busy making other plans', 'Many of life’s failures are people who did not realize how close they were to success when they gave up', 'The purpose of our lives is to be happy', 'Money and success don’t change people; they merely amplify what is already there.', 'Never let the fear of striking out keep you from playing the game', 'The big lesson in life, baby, is never be scared of anyone or anything', 'The whole secret of a successful life is to find out what is one’s destiny to do, and then do it.', 'The unexamined life is not worth living', 'Turn your wounds into wisdom.', 'Don’t settle for what life gives you; make life better and build something', 'Live for each second without hesitation'];
+                                    $random_keys = rand(0, sizeof($a) - 1);
+                                    echo $a[$random_keys];
+                                    ?>
                                 </p>
 
                                 <a href="{{ route('admin_dashboard') }}" class="btn btn-sm btn-outline-primary">Read More</a>
@@ -31,8 +31,38 @@
                     </div>
                 </div>
             </div>
-            
+
         </div>
-        
+
+        @if (isset($dashboardData))
+            <div class="row">
+                <div class="col-lg-4 col-md-12 col-6 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <span class="fw-semibold d-block mb-1">Pending Applications</span>
+                            <h3 class="card-title mb-2">{{ $dashboardData['pendingApplication'] }}</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12 col-6 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <span class="fw-semibold d-block mb-1">Approved Applications</span>
+                            <h3 class="card-title mb-2">{{ $dashboardData['approvedApplication'] }}</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12 col-6 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <span class="fw-semibold d-block mb-1">Rejected Applications</span>
+                            <h3 class="card-title mb-2">{{ $dashboardData['rejectedApplication'] }}</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
+
+
     </div>
 @endsection
